725d2baf93ab7ae7e234b62b43a2c95402ae0bc3
Joel Matias Duran <55359469+joelmatiasduran@users.noreply.github.com>
2021-04-08 21:23:56 -0300

    Adding Custom Fonts
    Loading Custom Fonts using Async:
    -LuckiestGuy
    -Krub
    -Importing "AppLoading"  to loading while the fonts load

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import RootNavigation from './src/navigation/RootNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { logoutUser } from './src/utils/networking/API';
 import { YellowBox } from 'react-native';
+import { AppLoading} from 'expo';
+import * as Font from 'expo-font'
+
+const getFonts = () => Font.loadAsync({
+  'Krub-Regular': require('./assets/fonts/Krub-Regular.ttf'),
+  'LuckiestGuy-Regular': require('./assets/fonts/LuckiestGuy-Regular.ttf')
+});
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [userId, setUserId] = useState(null);
 
@@ -19,6 +27,7 @@ export default function App() {
   };
 
   YellowBox.ignoreWarnings(['Non-serializable values were found in the navigation state']);
+  if (fontsLoaded){
 
   return (
     <NavigationContainer>
@@ -31,4 +40,13 @@ export default function App() {
       />
     </NavigationContainer>
   );
+}  else {
+
+  return (<AppLoading
+    startAsync={getFonts}
+    onFinish={()=> setFontsLoaded(true)}
+  />
+  )
+}
+
 }
